04cef83892066850af56ef8ae77a459910edbea8
tonsojun <josephjun@gmail.com>
2021-07-09 07:09:46 -0700

    Created react app, wired up forecast component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Forecast from './components/forecast/Forecast';
 
 function App() {
+
+  // const apiKey = process.env.WEATHER_APP_API_KEY;
+  // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Forecast />
     </div>
   );
 }
---
+++ src/components/forecast/Forecast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Forecast extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather Forecast for today</h1>
+      </div>
+    )
+  }
+}
+
+export default Forecast;
